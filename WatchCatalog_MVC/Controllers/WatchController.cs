1542573b7f351846888f476af24bacb8c05fe36b
Ludwig Ivan Comiso <comisoivan@gmail.com>
2023-08-14 13:23:52 +0800

    Add project files.

---
+++ WatchCatalog_MVC/Controllers/WatchController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WatchCatalog_MVC.Controllers
+{
+    public class WatchController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
