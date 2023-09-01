5a9c2117b7594b675de2de5d5a047df3f34f4d8e
Ludwig Ivan Comiso <comisoivan@gmail.com>
2023-09-01 16:44:25 +0800

    try on laptop

--- WatchCatalog_MVC/wwwroot/js/GlobalScripts.js
+++ WatchCatalog_MVC/wwwroot/js/GlobalScripts.js
@@ -34,3 +34,24 @@
         }
     })
 }
+
+
+const createShimmers = (shimmerCount) => {
+    let shimmers = []
+    for (let i = 0; i < shimmerCount; i++) {
+        const card = $("<div>").addClass("card shadow-none").css("width", "16vw").css({ "height": "42vh", "cursor": "pointer", "border": "none" })
+        const imgContainer = $("<div>").addClass("card").css("width", "100%").css({ "height": "100%", "position": "relative", "overflow": "hidden", "display": "flex", "justify-content": "center", "align-items": "center", "border": "none" })
+        const image = $(`<video autoplay loop muted playsinline src="/videos/shimmer.mp4">`).css({ "width": "130%" }).css("position", "absolute").css("margin", "auto")
+        card.append(imgContainer.append(image))
+
+        shimmers.push(card)
+    }
+
+    return shimmers;
+}
+
+const createRandomRate = () => {
+    let rating = $("<p>").text((Math.round(Math.random() * (5 - 4.56) + 4.56 * 100) / 100).toFixed(2)).css({ "position": "absolute", "top": ".5vh", "right": ".5vw", "font-size": "1.5vh" }).prepend('<img src="/images/star.png" style="height: 1.3vh; position: absolute; right: 1.6vw; top: .3vh"/>')
+
+    return rating
+}
